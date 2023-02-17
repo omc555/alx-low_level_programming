e1b89e364902e535cd4b7d71b4eea87dfe26100f
prosper <ogohiprosper@gmail.com>
2023-02-16 17:48:26 -0800

    okay

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
-#include <stdio.h>  
+#include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (success)
+ * main - A program that can print a line with puts function
+ * Return: 0
  */
-
 int main(void)
 {
-	
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
